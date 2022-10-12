d73be7d5331914196f594caf56e4c015fd38b8e8
jschuyl <schuylertjed@gmail.com>
2022-10-11 20:31:57 -0700

    I think the async change worked, gonna get the other roles set before starting tests

--- index.js
+++ index.js
@@ -4,63 +4,54 @@ const Manager = require("./assets/js/classes/manager");
 
 const newStaffData = [];
 
-const questions = [
-    {
-        type: "input",
-        message: "Please enter your name:",
-        name: "name"
-    },
-    {
-        type: "input",
-        message: "Please enter your ID number:",
-        name: "id"
-    },
-    {
-        type: "input",
-        message: "Please enter your email:",
-        name: "email"
-    },
-    {
-        type: "list",
-        message: "Choose your role from the following:",
-        choices: ["Manager", "Engineer", "Intern"],
-        name: "role"
-    }
-]
-if (answers.role === "Manager") {
-    managerQuestion = await inquirer
-        .prompt([
+const questions = async () => {
+    const answers = await inquirer
+    .prompt([
             {
+                type: "input",
+                message: "Please enter your name:",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "Please enter your ID number:",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "Please enter your email:",
+                name: "email"
+            },
+            {
+                type: "list",
+                message: "Choose your role from the following:",
+                choices: ["Manager", "Engineer", "Intern"],
+                name: "role"
+            }
+    ])
+    if (answers.role === "Manager") {
+        managerQuestion = await inquirer
+            .prompt([
+                 {
                     type: "input",
                     message: "what is your office number?",
                     name: "office"
                 }
-        ])   
+            ])   
 
-    const addManager = new Manager(
-        userAnswers.id,
-        userAnswers.name,
-        userAnswers.email,
-        managerAnswers.office
-    );
-    newStaffData.push(addManager)
-}
-
-
-
-
-
-async function readyStart() {
-    await askTheQuestions()
+        const addManager = new Manager(
+            answers.id,
+            answers.name,
+            answers.email,
+            managerQuestion.office
+        );
+        newStaffData.push(addManager)
+        console.log(newStaffData)
+    }
 }
 
 async function askTheQuestions() {
-    try {
-        const userAnswers = await inquirer.prompt(questions);
-        console.log("What you told me: ", userAnswers)
-        } catch (error) {
-            console.log(error)
-        }
-    }
+    await questions()
+}
+askTheQuestions();
 
-readyStart()
