ba0c7e9f1d3a67d2f79b5f7995681ca21bba4de7
jschuyl <schuylertjed@gmail.com>
2022-10-11 20:09:11 -0700

    tried to set up questions for the Manager, doesn't look like I can set it up like askTheQuestions

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
 const fs = require("fs")
 const inquirer = require("inquirer");
+const Manager = require("./assets/js/classes/manager");
 
-const openingQuestion = [
+const newStaffData = [];
+
+const questions = [
+    {
+        type: "input",
+        message: "Please enter your name:",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "Please enter your ID number:",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "Please enter your email:",
+        name: "email"
+    },
     {
         type: "list",
-        message: "What is this employee's role?",
+        message: "Choose your role from the following:",
         choices: ["Manager", "Engineer", "Intern"],
         name: "role"
-    },
-
+    }
 ]
+if (answers.role === "Manager") {
+    managerQuestion = {
+        type: "input",
+        message: "what is your office number?",
+        name: "office"
+    }
+    const managerAnswers = await inquirer.prompt(managerQuestion)
 
+    addManager = new Manager(
+        answers.id,
+        answers.name,
+        answers.email,
+        managerAnswers.office
+    );
+    newStaffData.push(addManager)
+}
 
 
 
@@ -20,7 +52,7 @@ const openingQuestion = [
 
 async function askTheQuestions() {
     try {
-        const userAnswers = await inquirer.prompt(openingQuestion);
+        const userAnswers = await inquirer.prompt(questions);
         console.log("What you told me: ", userAnswers)
         } catch (error) {
             console.log(error)
