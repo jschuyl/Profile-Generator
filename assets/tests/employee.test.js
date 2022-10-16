741bc0a3f7020a1129ef87ffe50fbb2c560c059a
jschuyl <schuylertjed@gmail.com>
2022-10-15 17:51:07 -0700

    found out why tests were not working! tests are starting to come along

--- assets/tests/employee.test.js
+++ assets/tests/employee.test.js
@@ -1,2 +1,24 @@
 const Employee = require("../js/classes/employee");
 
+describe("Employee", () => {
+    describe("Initialization", () => {
+        it("Should return an object upon initialization containing name, id, and email", () => {
+            let obj = new Employee();
+            expect('name' in obj).toEqual(true);
+            expect('id' in obj).toEqual(true);
+            expect('email' in obj).toEqual(true);
+        })
+    })
+});
+
+it("sets name, id, and email to true", () => {
+    let name = "beans";
+    let id = 96;
+    let email = "beansandtoast@gmail.com";
+    let obj = new Employee(id, name, email)
+
+    expect(obj.name).toEqual(name);
+    expect(obj.id).toEqual(id);
+    expect(obj.email).toEqual(email);
+});
+
